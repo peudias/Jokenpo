094f1d49fc094f7644c4f4005f187d7e99af225c
p8 <phpdias@outlook.com>
2022-10-21 19:18:19 -0300

    Update Game.dart

--- lib/Game.dart
+++ lib/Game.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Game extends StatefulWidget {
   const Game({Key? key}) : super(key: key);
@@ -8,10 +9,69 @@ class Game extends StatefulWidget {
 }
 
 class _GameState extends State<Game> {
+
+  var _imageApp = AssetImage("images/default.png");
+  var _message = "Choose your move:";
+
+  _SelectedOption(String SelectedUserOption){
+
+    var options = ["paper", "rock", "scissor"];
+    var number = Random().nextInt(3);
+    var appChoice = options[number];
+
+    //display of the image chosen by the app
+    switch(appChoice){
+      case "paper":
+        setState(() {
+          this._imageApp = AssetImage("images/paper.png");
+        });
+        break;
+
+      case "rock":
+        setState(() {
+          this._imageApp = AssetImage("images/rock.png");
+        });
+        break;
+
+      case "scissor":
+        setState(() {
+          this._imageApp = AssetImage("images/scissor.png");
+        });
+        break;
+    }
+
+    //Winner check
+    //user wins
+    if(
+        (SelectedUserOption == "rock" && appChoice == "scissor") ||
+        (SelectedUserOption == "scissor" && appChoice == "paper") ||
+        (SelectedUserOption == "paper" && appChoice == "rock")
+    ){
+      setState(() {
+        this._message = "Congratulations!! You won!";
+      });
+    //app wins
+    }else if(
+        (SelectedUserOption == "rock" && appChoice == "paper") ||
+        (SelectedUserOption == "scissor" && appChoice == "rock") ||
+        (SelectedUserOption == "paper" && appChoice == "scissor")
+    ){
+      setState(() {
+        this._message = "You lost. Can machine thinks?";
+      });
+    }else{
+      this._message = "Until you're not so bad... tie!";
+    }
+
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amberAccent,
       appBar: AppBar(
+        backgroundColor: Colors.orange,
         title: Text("JokenPo"),
       ),
 
@@ -36,8 +96,8 @@ class _GameState extends State<Game> {
 
 
         //2) app's choose image
-        Image.asset(
-          "images/default.png",
+        Image(
+          image: this._imageApp,
           width: 125,
         ),
 
@@ -46,7 +106,7 @@ class _GameState extends State<Game> {
         Padding(
           padding: EdgeInsets.only(top: 32, bottom: 16),
           child: Text(
-            "Choose your move:",
+            this._message,
             textAlign: TextAlign.center,
             style: TextStyle(
                 fontSize: 35,
@@ -58,27 +118,71 @@ class _GameState extends State<Game> {
         ),
 
 
+
         //4) 3 images (paper, rock and scissor)
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Image.asset(
-              "images/paper.png",
-              width: 100,
-            ),
-            Image.asset(
-              "images/rock.png",
-              width: 100,
-            ),
-            Image.asset(
-              "images/scissor.png",
-              width: 100,
-            ),
+           GestureDetector(
+             onTap: () => _SelectedOption("paper"),
+             child: Image.asset(
+               "images/paper.png",
+               width: 100,
+             ),
+           ),
+
+           GestureDetector(
+             onTap: () => _SelectedOption("rock"),
+             child: Image.asset(
+               "images/rock.png",
+               width: 100,
+             ),
+           ),
+
+           GestureDetector(
+             onTap: () => _SelectedOption("scissor"),
+             child: Image.asset(
+               "images/scissor.png",
+               width: 100,
+             ),
+           ),
           ],
         ),
 
 
+      //5) Naruto's reference
+      Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Good Luck!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                     fontSize: 35,
+                     fontWeight: FontWeight.bold,
+                     fontStyle: FontStyle.italic,
+                     color: Colors.black
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+
+
+          Container(
+            padding: EdgeInsets.all(5),
+            width: double.infinity,
+            color: Colors.deepOrangeAccent,
+            child: Image.asset("images/jokenpo.gif"),
+          ),
         ],
+      ),
+      ],
       ),
     );
   }
