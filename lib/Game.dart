e7d150a650fc6625e81709e1fe49d5e98a9d8fde
p8 <phpdias@outlook.com>
2022-10-22 16:12:42 -0300

    Update Game.dart

--- lib/Game.dart
+++ lib/Game.dart
@@ -75,114 +75,118 @@ class _GameState extends State<Game> {
         title: Text("JokenPo"),
       ),
 
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
 
-        //1) text
-        Padding(
-          padding: EdgeInsets.only(top: 32, bottom: 16),
-          child: Text(
-            "App's choice:",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 35,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
-                color: Colors.black
-            ),
-          ),
-        ),
-
-
-        //2) app's choose image
-        Image(
-          image: this._imageApp,
-          width: 125,
-        ),
-
-
-        //3) text resulted
-        Padding(
-          padding: EdgeInsets.only(top: 32, bottom: 16),
-          child: Text(
-            this._message,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 35,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
-                color: Colors.black
-            ),
-          ),
-        ),
-
-
-
-        //4) 3 images (paper, rock and scissor)
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-           GestureDetector(
-             onTap: () => _SelectedOption("paper"),
-             child: Image.asset(
-               "images/paper.png",
-               width: 100,
-             ),
-           ),
-
-           GestureDetector(
-             onTap: () => _SelectedOption("rock"),
-             child: Image.asset(
-               "images/rock.png",
-               width: 100,
-             ),
-           ),
-
-           GestureDetector(
-             onTap: () => _SelectedOption("scissor"),
-             child: Image.asset(
-               "images/scissor.png",
-               width: 100,
-             ),
-           ),
-          ],
-        ),
-
-
-      //5) Naruto's reference
-      Column(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(top: 32, bottom: 16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  "Good Luck!",
+              //1) text
+              Padding(
+                padding: EdgeInsets.only(top: 32, bottom: 16),
+                child: Text(
+                  "App's choice:",
                   textAlign: TextAlign.center,
                   style: TextStyle(
-                     fontSize: 35,
-                     fontWeight: FontWeight.bold,
-                     fontStyle: FontStyle.italic,
-                     color: Colors.black
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.black
                   ),
                 ),
-              ],
-            ),
+              ),
+
+
+              //2) app's choose image
+              Image(
+                image: this._imageApp,
+                width: 125,
+              ),
+
+
+              //3) text resulted
+              Padding(
+                padding: EdgeInsets.only(top: 32, bottom: 16),
+                child: Text(
+                  this._message,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.black
+                  ),
+                ),
+              ),
+
+
+
+              //4) 3 images (paper, rock and scissor)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () => _SelectedOption("paper"),
+                    child: Image.asset(
+                      "images/paper.png",
+                      width: 100,
+                    ),
+                  ),
+
+                  GestureDetector(
+                    onTap: () => _SelectedOption("rock"),
+                    child: Image.asset(
+                      "images/rock.png",
+                      width: 100,
+                    ),
+                  ),
+
+                  GestureDetector(
+                    onTap: () => _SelectedOption("scissor"),
+                    child: Image.asset(
+                      "images/scissor.png",
+                      width: 100,
+                    ),
+                  ),
+                ],
+              ),
+
+
+              //5) Naruto's reference
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top: 32, bottom: 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Good Luck!",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic,
+                              color: Colors.black
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+
+
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    width: double.infinity,
+                    color: Colors.deepOrangeAccent,
+                    child: Image.asset("images/jokenpo.gif"),
+                  ),
+                ],
+              ),
+            ],
           ),
-
-
-
-          Container(
-            padding: EdgeInsets.all(5),
-            width: double.infinity,
-            color: Colors.deepOrangeAccent,
-            child: Image.asset("images/jokenpo.gif"),
-          ),
-        ],
-      ),
-      ],
+        ),
       ),
     );
   }
